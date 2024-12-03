93f59bb9732ebac51c2deb10cf047b703ca8fb6c
Mark Moxon <mark@moxon.net>
2024-12-03 15:58:08 +0000

    Add Commodore 64 and Apple II to code comment extraction script

--- code-analysis/extract-text-code-comments/code-comments.sh
+++ code-analysis/extract-text-code-comments/code-comments.sh
@@ -2,6 +2,8 @@ find $ELITE_CODE_REPOSITORIES/elite-source-code-bbc-micro-cassette/1-source-file
 find $ELITE_CODE_REPOSITORIES/elite-source-code-bbc-micro-disc/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> elite.txt
 find $ELITE_CODE_REPOSITORIES/elite-source-code-6502-second-processor/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> elite.txt
 find $ELITE_CODE_REPOSITORIES/elite-source-code-acorn-electron/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> elite.txt
+find $ELITE_CODE_REPOSITORIES/elite-source-code-commodore-64/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code_nes.pl {} \; >> elite.txt
+find $ELITE_CODE_REPOSITORIES/elite-source-code-apple-ii/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code_nes.pl {} \; >> elite.txt
 find $ELITE_CODE_REPOSITORIES/elite-source-code-bbc-master/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> elite.txt
 find $ELITE_CODE_REPOSITORIES/elite-a-source-code-bbc-micro/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> elite.txt
 find $ELITE_CODE_REPOSITORIES/elite-source-code-nes/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code_nes.pl {} \; >> elite.txt
