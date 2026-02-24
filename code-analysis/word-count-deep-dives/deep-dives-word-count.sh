25e0464db422fe37497b389a7545619d4748277f
Mark Moxon <mark@moxon.net>
2026-02-24 09:55:30 +0000

    Add The Sentinel to deep dive word count

--- code-analysis/word-count-deep-dives/deep-dives-word-count.sh
+++ code-analysis/word-count-deep-dives/deep-dives-word-count.sh
@@ -4,4 +4,6 @@ find $REVS_WEBSITE/deep_dives -maxdepth 1 -name '*.html' -type f -exec perl word
 
 find $AVIATOR_WEBSITE/deep_dives -maxdepth 1 -name '*.html' -type f -exec perl word_count_html.pl {} \; | sort -Vr >> wordcount.txt
 
+find $THE_SENTINEL_WEBSITE/deep_dives -maxdepth 1 -name '*.html' -type f -exec perl word_count_html.pl {} \; | sort -Vr >> wordcount.txt
+
 find $LANDER_WEBSITE/deep_dives -maxdepth 1 -name '*.html' -type f -exec perl word_count_html.pl {} \; | sort -Vr >> wordcount.txt
