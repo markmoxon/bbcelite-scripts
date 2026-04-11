3ccea310437954b91f5f1e6dc419d1a0273deee2
Mark Moxon <mark@moxon.net>
2026-04-11 16:11:23 +0100

    Apply fix for space in The Sentinel path

--- code-analysis/word-count-code-comments/code-comments-word-count.sh
+++ code-analysis/word-count-code-comments/code-comments-word-count.sh
@@ -10,12 +10,7 @@ find $ELITE_CODE_REPOSITORIES/elite-a-source-code-bbc-micro/1-source-files/main-
 find $ELITE_CODE_REPOSITORIES/elite-source-code-nes/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec wc {} \; >> temp.txt
 find $AVIATOR_CODE_REPOSITORY/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec wc {} \; >> temp.txt
 find $REVS_CODE_REPOSITORY/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec wc {} \; >> temp.txt
-
-saveIFS="$IFS"
-IFS=$'\n'
-find $THE_SENTINEL_CODE_REPOSITORY/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec wc {} \; >> temp.txt
-IFS="$saveIFS"
-
+find "$THE_SENTINEL_CODE_REPOSITORY/1-source-files/main-sources" -maxdepth 1 -name '*.asm' -type f -exec wc {} \; >> temp.txt
 find $LANDER_CODE_REPOSITORY/1-source-files/main-sources -maxdepth 1 -name '*.arm' -type f -exec wc {} \; >> temp.txt
 
 perl word_count_convert.pl temp.txt > linecount.txt
@@ -31,12 +26,7 @@ find $ELITE_CODE_REPOSITORIES/elite-a-source-code-bbc-micro/1-source-files/main-
 find $ELITE_CODE_REPOSITORIES/elite-source-code-nes/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code_nes.pl {} \; >> temp.txt
 find $AVIATOR_CODE_REPOSITORY/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> temp.txt
 find $REVS_CODE_REPOSITORY/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> temp.txt
-
-saveIFS="$IFS"
-IFS=$'\n'
-find $THE_SENTINEL_CODE_REPOSITORY/1-source-files/main-sources -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> temp.txt
-IFS="$saveIFS"
-
+find "$THE_SENTINEL_CODE_REPOSITORY/1-source-files/main-sources" -maxdepth 1 -name '*.asm' -type f -exec perl strip_code.pl {} \; >> temp.txt
 find $LANDER_CODE_REPOSITORY/1-source-files/main-sources -maxdepth 1 -name '*.arm' -type f -exec perl strip_code.pl {} \; >> temp.txt
 
 perl word_count_convert.pl temp.txt > wordcount.txt
