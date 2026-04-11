f475e99f9326d618495dc684800cd689ad399abc
Mark Moxon <mark@moxon.net>
2026-04-11 14:52:13 +0100

    Add The Sentinel to deep dive text extraction tool

--- code-analysis/extract-text-deep-dives/deep-dives.sh
+++ code-analysis/extract-text-deep-dives/deep-dives.sh
@@ -4,4 +4,6 @@ find $REVS_WEBSITE/deep_dives -maxdepth 1 -name '*.html' -type f -exec perl stri
 
 find $AVIATOR_WEBSITE/deep_dives -maxdepth 1 -name '*.html' -type f -exec perl strip_html.pl {} \; > aviator.txt
 
+find $THE_SENTINEL_WEBSITE/deep_dives -maxdepth 1 -name '*.html' -type f -exec perl strip_html.pl {} \; > the-sentinel.txt
+
 find $LANDER_WEBSITE/deep_dives -maxdepth 1 -name '*.html' -type f -exec perl strip_html.pl {} \; > lander.txt
